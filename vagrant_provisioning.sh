deb02a3350b2fffa994a964e7eb080a8f06a365d
Sebastian Haas <sebastian.haas@grape.at>
2015-09-17 14:22:52 +0200

    Added vagrant support.

---
+++ vagrant_provisioning.sh
@@ -0,0 +1,30 @@
+echo "Starting provisioning..."
+
+if [[ $(ldconfig -p | grep libcmocka) ]]; then
+    echo "Found library \"cmocka\"."
+else
+    echo "Installing cmocka unit test framework..."
+	sudo apt-get -qq -y --no-install-recommends install cmake
+	sudo apt-get -qq -y --no-install-recommends install git
+	git clone --branch cmocka-1.0.1 https://git.cryptomilk.org/projects/cmocka.git
+	cd cmocka
+	mkdir build
+	cd build
+	cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug ..
+	make
+	sudo make install
+	sudo ldconfig
+	cd ~
+	echo "Finished."
+fi
+
+echo "Running unit tests..."
+cd /vagrant
+make clean
+make
+./piclibtest
+make clean
+cd ~
+echo "Finished unit test run."
+
+echo "Finished provisioning. Your machine is ready to use."
